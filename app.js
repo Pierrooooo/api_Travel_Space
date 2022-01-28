d9b2dd4d51375f9fee9ab078ebb914d85d215f0f
Pierre Caillet <pierrE.caillet@edu.devinci.fr>
2022-01-28 23:12:14 +0100

    :sparkles: Onclick of planet and starship, it goes to your travel infos

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ function getPlanets(){
 
         document.querySelectorAll('.planet').forEach(planet => {
             planet.addEventListener('click', event => {
-                yourDes.innerHTML += `
-                <p>${planet.name}</p>
+                console.log(planet)
+                document.querySelector('.your_destination').innerHTML += `
+                <p>${planet.dataset.name}</p>
                 `
                 getStarShips()
             })
@@ -45,7 +46,7 @@ function getStarShips(){
 
         response.results.forEach(ship => {
             starShipsDiv.innerHTML += `
-            <div class="ship">
+            <div class="ship" data-name="${ship.name}">
                 <p>${ship.name}</p>
             </div>
             `
@@ -53,26 +54,40 @@ function getStarShips(){
 
         document.querySelectorAll('.ship').forEach(ship => {
             ship.addEventListener('click', el => {  
-                getPeoples()
-                getCharacters()
+
+                document.querySelector('.your_starship').innerHTML += `
+                <p>${ship.dataset.name}</p>
+                `
+
+                document.querySelector('body').innerHTML +=`
+                <section class="choose_universe">
+                    <h2>Choose an Universe</2>
+                    <button onclick="getStarWarsCharacters()">Star Wars</button>
+                    <button onclick="getGOTCharacters()">Game Of Thrones</button>
+                    <button onclick="getRMCharacters()">Rick & Morty</button>
+                    <button onclick="getHPCharacters()">Harry Potter</button>
+                </section>
+                <div class="all_char"></div>
+                `
             
             })
         })
     })
 }
 
-function getPeoples(){
+function getStarWarsCharacters(){
+    document.querySelector('.all_char').innerHTML=``
 
     fetch('https://swapi.dev/api/people')
     .then(response => response.json())
     .then(response => {
-        document.querySelector('body').innerHTML += `<section class="peoples_div"></section>`
-        let peoplesDiv = document.querySelector('.peoples_div')
+        document.querySelector('.all_char').innerHTML += `<section class="characters_starwars_div"></section>`
+        let peoplesDiv = document.querySelector('.characters_starwars_div')
         console.log(peoplesDiv)
 
         response.results.forEach(people => {
             peoplesDiv.innerHTML += `
-            <div class="people">
+            <div class="people" data-name="${people.name}">
                 <p>${people.name}</p>
             </div>
             `
@@ -80,19 +95,19 @@ function getPeoples(){
         
     })
 }
-function getCharacters(){
+function getGOTCharacters(){
+    document.querySelector('.all_char').innerHTML=``
     
-
     fetch('https://thronesapi.com/api/v2/Characters')
     .then(response => response.json())
     .then(response => {
-        document.querySelector('body').innerHTML += `<section class="characters_div"></section>`
-        let charactersDiv = document.querySelector('.characters_div')
+        document.querySelector('.all_char').innerHTML += `<section class="characters_got_div"></section>`
+        let charactersDiv = document.querySelector('.characters_got_div')
         console.log(charactersDiv)
 
         response.forEach(characters => {
             charactersDiv.innerHTML += `
-            <div class="people">
+            <div class="people" data-name="${characters.fullName}" data-image="${characters.imageUrl}">
                 <img src="${characters.imageUrl}"></img>
                 <p>${characters.fullName}, ${characters.family}</p>
             </div>
@@ -101,8 +116,51 @@ function getCharacters(){
         
     })
 }
+function getRMCharacters(){
+    document.querySelector('.all_char').innerHTML=``
+    
+    fetch('https://rickandmortyapi.com/api/character')
+    .then(response => response.json())
+    .then(response => {
+        document.querySelector('.all_char').innerHTML += `<section class="characters_rm_div"></section>`
+        let charactersDiv = document.querySelector('.characters_rm_div')
+        console.log(charactersDiv)
 
-// the witcher Sims films disney 
-//https://rickandmortyapi.com/api/character
+        response.results.forEach(characters => {
+            charactersDiv.innerHTML += `
+            <div class="people" data-name="${characters.name}" data-image="${characters.image}">
+                <img src="${characters.image}"></img>
+                <p>${characters.name}, ${characters.species}</p>
+            </div>
+            `
+        })
+        
+    })
+}
+
+function getHPCharacters(){
+    document.querySelector('.all_char').innerHTML=``
+    
+    fetch('https://hp-api.herokuapp.com/api/characters')
+    .then(response => response.json())
+    .then(response => {
+        document.querySelector('.all_char').innerHTML += `<section class="characters_hp_div"></section>`
+        let charactersDiv = document.querySelector('.characters_hp_div')
+        console.log(charactersDiv)
+
+        response.forEach(characters => {
+            charactersDiv.innerHTML += `
+            <div class="people" data-name="${characters.name}" data-image="${characters.image}">
+                <img src="${characters.image}"></img>
+                <p>${characters.name}, ${characters.species}</p>
+            </div>
+            `
+        })
+        
+    })
+}
+
+// the witcher films disney 
 //https://hp-api.herokuapp.com/api/characters
 // https://api.disneyapi.dev/characters
+//https://rickandmortyapi.com/api/location
